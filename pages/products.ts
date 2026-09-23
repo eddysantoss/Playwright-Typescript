f36c232676be325d4b4b88c558286d697797d34d
eddyclayton@gmail.com <eddyclayton@gmail.com>
2026-09-23 15:30:21 -0300

    Migrate checkout page to TypeScript

--- pages/products.ts
+++ pages/products.ts
@@ -1,37 +1,38 @@
-import { expect } from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
 
-exports.ProductsPage = class ProductsPage {
-    constructor(page) {
+export class ProductsPage {
+    private readonly page: Page;
+    private readonly cartBadge: Locator;
+    private readonly cartLink: Locator;
 
+    constructor(page: Page) {
         this.page = page;
         this.cartBadge = page.locator('.shopping_cart_badge');
         this.cartLink = page.locator('[data-test="shopping-cart-link"]');
-
     }
 
-    async gotoProductsPage() {
+    async gotoProductsPage(): Promise<void> {
         await this.page.goto('https://www.saucedemo.com/inventory.html');
     }
 
-    async addProductToCart(productName) {
+    async addProductToCart(productName: string): Promise<void> {
         await this.page.click(`button[data-test="add-to-cart-${productName}"]`);
         await expect(this.cartBadge).toHaveText('1');
     }
 
-    async removeProductFromCart(productName) {
+    async removeProductFromCart(productName: string): Promise<void> {
         await this.page.click(`button[data-test="remove-${productName}"]`);
         await expect(this.cartBadge).toHaveCount(0);
     }
 
-    async goToCart() {
+    async goToCart(): Promise<void> {
         await this.cartLink.click();
     }
 
-    async goToCartAndRemove(productName) {
+    async goToCartAndRemove(productName: string): Promise<void> {
         await this.page.click(`button[data-test="add-to-cart-${productName}"]`);
         await this.cartLink.click();
         await this.removeProductFromCart(productName);
         await expect(this.cartBadge).toHaveCount(0);
-
     }
 }
